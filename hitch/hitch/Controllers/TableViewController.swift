2326fbe3c7e1f71c30bad4391daf205a1be17b30
ben-cunningham <bencunningham17@gmail.com>
2016-03-12 12:13:24 -0800

    Add right bar button

--- hitch/hitch/Controllers/TableViewController.swift
+++ hitch/hitch/Controllers/TableViewController.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+//    var newRideButton: UIBarButtonItem
+    
     init() {
         super.init(style: .Plain)
+    }
+    
+    override func viewDidLoad() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: Selector("newRide"))
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: TableViewController delegate methods
     
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return UITableViewCell(style: .Default, reuseIdentifier: nil)
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func newRide() {
+        print("button pressed")
+    }
+    
+    func requestForRides() {
+        
+    }
 }
