aa47cee5948e540d9bd6d9cdcc446b5c63fe63f4
ben-cunningham <bencunningham17@gmail.com>
2016-03-12 11:59:59 -0800

    Add tableViewController

---
+++ hitch/hitch/Controllers/TableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewController.swift
+//  hitch
+//
+//  Created by Benjamin Cunningham on 2016-03-12.
+//  Copyright © 2016 Benjamin Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    init() {
+        super.init(style: .Plain)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
